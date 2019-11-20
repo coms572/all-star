f38488fe054c859c870c9578655ab92adf617ab3
Ben Steenhoek <benjis@iastate.edu>
2019-11-20 15:00:22 -0600

    data.csv

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,16 +11,3 @@ a = 0.5
 b = 1
 c= 2
 d = 3
- 
-#GoalKeeping Characterstics
-df['gk_Shot_Stopper'] = (b*df.Reactions + b*df.Composure + a*df.Speed + a*df.Strength + c*df.Jumping + b*df.GK_Positioning + c*df.GK_Diving + d*df.GK_Reflexes + b*df.GK_Handling)/(2*a + 4*b + 2*c + 1*d)
-df['gk_Sweeper'] = (b*df.Reactions + b*df.Composure + b*df.Speed + a*df.Short_Pass + a*df.Long_Pass + b*df.Jumping + b*df.GK_Positioning + b*df.GK_Diving + d*df.GK_Reflexes + b*df.GK_Handling + d*df.GK_Kicking + c*df.Vision)/(2*a + 4*b + 3*c + 2*d)
-
-plt.figure(figsize=(15,6))
- 
-# Generate sequential data and plot
-sd = df.sort_values('gk_Shot_Stopper', ascending=False)[:5]
-x1 = np.array(list(sd['Name']))
-y1 = np.array(list(sd['gk_Shot_Stopper']))
-sns.barplot(x1, y1, palette= "colorblind")
-plt.ylabel("Shot Stopping Score")
